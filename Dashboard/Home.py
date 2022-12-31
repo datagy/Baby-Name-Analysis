72c59a16a7e0410a2a6098451be0e406519de527
datagy <nik@datagy.io>
2022-12-31 17:45:31 -0500

    Resolved column name error

--- Dashboard/Home.py
+++ Dashboard/Home.py
@@ -12,4 +12,4 @@ def load_data():
 
 
 df = load_data()
-df['Proportion'] / df.groupby(['Year', 'Gender'])['Num'].transform('sum')
+df['Proportion'] / df.groupby(['Year', 'Gender'])['Number'].transform('sum')
