b344614be766a0a310bb889b28f9fbd3ad1e8539
datagy <nik@datagy.io>
2023-01-01 11:03:56 -0500

    Resolved caching issue

--- Dashboard/Home.py
+++ Dashboard/Home.py
@@ -9,11 +9,13 @@ st.sidebar.markdown("# Home")
 
 @st.cache
 def load_data():
-    return pd.read_csv('names.csv')
+    df = pd.read_csv('names.csv')
+    df['Proportion'] = df['Number'] / \
+        df.groupby(['Year', 'Gender'])['Number'].transform('sum')
+
+    df['Gender'] = df['Gender'].map({'M': 'Male', 'F': 'Female'})
+
+    return df
 
 
 df = load_data()
-df['Proportion'] = df['Number'] / \
-    df.groupby(['Year', 'Gender'])['Number'].transform('sum')
-
-df['Gender'] = df['Gender'].map({'M': 'Male', 'F': 'Female'})
